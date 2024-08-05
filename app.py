421d7e3db7436e8ea971c0af2c7207ab24014083
jyun27 <kimjyun27@gmail.com>
2024-08-05 10:20:19 +0900

    first commit

--- app.py
+++ app.py
@@ -1,14 +1,21 @@
 import os
+import pandas as pd
+import openai
+import json
 from dotenv import load_dotenv
 from flask import Flask, render_template, request, Response
-import openai
-from openai.error import RateLimitError
+from openai import OpenAIError, RateLimitError
+from langchain_community.chat_models import ChatOpenAI
+from langchain_experimental.agents import create_pandas_dataframe_agent
+from langchain.agents.agent_types import AgentType
+from langchain_openai import ChatOpenAI 
 
 load_dotenv()
 
 app = Flask(__name__)
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
+df = pd.read_excel("ReChat_data.xlsx")
 
 @app.route('/')
 def index():
@@ -18,16 +25,34 @@ def index():
 def generate(messages, model_type):
     def stream():
         try:
-            response = openai.ChatCompletion.create(
-                model=model_type,
-                messages=messages,
-                stream=True
-            )
-
+            # 에이전트 생성
+            agent = create_pandas_dataframe_agent(
+            ChatOpenAI(temperature=0, model=model_type), # 모델 정의
+            df,                                            # 데이터프레임
+            verbose=True,                                  # 추론과정 출력
+            agent_type=AgentType.OPENAI_FUNCTIONS,         # 에이전트 타입
+            allow_dangerous_code=True                      # 위험한 코드 실행 허용
+         ) # 에이전트를 통해 메시지 처리
+            
+            response = agent.run(messages) 
+            
+            full_message = ""
             for chunk in response:
-                content = chunk['choices'][0]['delta'].get('content', '')
-                if content:
-                    yield content
+                if not chunk:  # Skip empty chunks
+                    continue
+                
+                try:
+                    if isinstance(chunk, str):
+                        data = json.loads(chunk)
+                        content = data['choices'][0]['delta'].get('content', '')
+                    elif isinstance(chunk, int):
+                        content = str(chunk)
+                    else:
+                        content = ""
+                    full_message += content
+                except (json.JSONDecodeError, TypeError):
+                    continue  # Skip invalid JSON chunks or type errors
+            yield full_message
 
         except RateLimitError:
             yield "The server is experiencing a high volume of requests. Please try again later."
